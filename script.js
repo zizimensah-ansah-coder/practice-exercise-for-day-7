8f247e4507b5140d85fb49cb7eb8b9145f0fb85c
zizimensah-ansah-coder <zizi. mensah-ansah@amalitechtraining.org>
2026-09-16 14:39:17 +0000

    Created a promise that resolves after 2 seconds and also created another promise that rejects and handles error

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
-//Creating a normalfunction
 
 
-//Arrow function of the normal function
-const square = number => number * number;
-console.log(square(5));
+//Creating a promise
+let p = new Promise((resolve,reject) => {
+    let a = 1 + 3
+    if(a == 2) {
+        resolve("Success")
+    }
+    else{
+        reject("Failed")
+    }
+})
 
-// Second Normal function
+p.then((message) => {
+    console.log("This is in the then " + message)
+}).catch((message)=> {
+    console.log("This is in the catch "  + message)
+})
 
+//promise that resolves after two seconds
+let pro= new  Promise((resolve) => {
+    setTimeout(()  => {
+        resolve("This promise will be resolved after two seconds");
+    },2000);
 
-//Arrow fuction 
-let x = 5;
-let y = 8;
-const addNumbers = numbers => x + y;
-console.log(x + y);
-
-//Greet function
-
-const greet = greet => "Good morning!";
-console.log(greet);
+});
+pro.then((message) => {
+    console.log( message);
+});
+//promise that rejects and handles error
+let rejectPromise = new Promise((reject)  => {
+    reject("Something went wrong");
+});
+rejectPromise.then((message)=> {
+    console.log(message)
+}).catch((error) => {
+    console.error("Error: ", error);
+});

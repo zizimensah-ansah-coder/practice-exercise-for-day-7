79f8c32e1e10a705f97355d610e62409a965019b
zizimensah-ansah-coder <zizi. mensah-ansah@amalitechtraining.org>
2026-09-16 10:28:01 +0000

    Coverted three normal functions into arrow functions

---
+++ script.js
@@ -0,0 +1,20 @@
+//Creating a normalfunction
+
+
+//Arrow function of the normal function
+const square = number => number * number;
+console.log(square(5));
+
+// Second Normal function
+
+
+//Arrow fuction 
+let x = 5;
+let y = 8;
+const addNumbers = numbers => x + y;
+console.log(x + y);
+
+//Greet function
+
+const greet = greet => "Good morning!";
+console.log(greet);

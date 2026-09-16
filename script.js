17c9039c24c7b1fb20e5987aa7f8381391add4c7
zizimensah-ansah-coder <zizi. mensah-ansah@amalitechtraining.org>
2026-09-16 15:35:09 +0000

    One example using .then()/.catch() and then rewriting it using async/await then trying catch

--- script.js
+++ script.js
@@ -1,66 +1,24 @@
-//Creating a normalfunction
-const square = function(square) {
-    return (square(5));
+//using asyns and await
+function getUser() {
+    return new Promise((resolve, reject) => {
+        
+            resolve("The data was downloaded")
+        });
+    
 };
-
-//Arrow function of the normal function
-const squares = number => number * number;
-console.log(square(5));
-
-// Second Normal function
-let x = 5;
-let y = 8;
-const addNumber = function(numbers) {
-    return x + y;
-};
-
-//Arrow fuction 
-let x1 = 5;
-let y1 = 8;
-const addNumbers = numbers => x1 + y1;
-console.log(x1 + y1);
-
-//Greet function
-const greet = function(greet) {
-    return greet;
-}
-//Arrow function
-const greets = greet => "Good morning!";
-console.log(greet);
-
-//Creating a promise
-let p = new Promise((resolve,reject) => {
-    let a = 1 + 3
-    if(a == 2) {
-        resolve("Success")
+getUser().then((message) => {
+    console.log(message);
+}).catch((error)  => {
+    console.error("Error: " ,error);
+});
+//using async and wait 
+async function loadUser() {
+    try{
+        const message = await getUser();
+        console.log(message);
     }
-    else{
-        reject("Failed")
+    catch(error) {
+        console.error(error)
     }
-})
-
-p.then((message) => {
-    console.log("This is in the then " + message)
-}).catch((message)=> {
-    console.log("This is in the catch "  + message)
-})
-
-//promise that resolves after two seconds
-let pro= new  Promise((resolve) => {
-    setTimeout(()  => {
-        resolve("This promise will be resolved after two seconds");
-    },2000);
-
-});
-pro.then((message) => {
-    console.log( message);
-});
-//promise that rejects and handles error
-let rejectPromise = new Promise((reject)  => {
-    reject("Something went wrong");
-});
-rejectPromise.then((message)=> {
-    console.log(message)
-}).catch((error) => {
-    console.error("Error: ", error);
-});
+    }
+    
